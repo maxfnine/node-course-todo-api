d7c0d902de201a4d8f67b235eae4b27e09b0f143
maxfnine <maxfnine@gmail.com>
2018-07-10 21:49:49 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,32 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose.js');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
+
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-var {text,completed,completedAt}=req.body;
-var todo = new Todo({text,completed,completedAt});
-todo.save().then((result)=>{
-    res.send(result);
-},(err)=>{
-    res.status(400).send(err);
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
-
-
-app.listen(3000,()=>{
-    console.log('Listening on port 3000');
-    
-});
-
-
-
-
-
+module.exports = {app};
