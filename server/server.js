372fa39cbcdbcbd2b5264a7fec6e0f7b2f7db326
maxfnine <35781896+maxfnine@users.noreply.github.com>
2018-07-10 14:40:49 +0300

    Validation of input in model

--- server/server.js
+++ server/server.js
@@ -5,30 +5,44 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 var Todo = mongoose.model('Todo',{
     text:{
-        type:String
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default:false
     },
     completedAt:{
         type:Number
     }
 });
 
+var User = mongoose.model('User',{
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:1
+    }
+});
+
 // var todo = new Todo({
-//     text:'Cook dinner'
+//     text:'Done with Mongoose',
+//     completed:true,
+//     completedAt:1000
 // });
 
 // todo.save().then((result)=>{console.log('Todo',JSON.stringify(result,undefined,2));
 // },(err)=>{console.log(err);
 // });
-var todo = new Todo({
-    text:'Done with Mongoose',
-    completed:true,
-    completedAt:1000
+
+var user = new User({
+    email:'maxfnine@gmail.com'
 });
 
-todo.save().then((result)=>{console.log('Todo',JSON.stringify(result,undefined,2));
+user.save().then((result)=>{console.log('User',JSON.stringify(result,undefined,2));
 },(err)=>{console.log(err);
 });
 
