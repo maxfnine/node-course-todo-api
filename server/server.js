b88be17125f996e493d5de3b0469ddfde3908795
maxfnine <35781896+maxfnine@users.noreply.github.com>
2018-07-12 16:34:28 +0300

    Adding GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID}=require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,6 +32,30 @@ Todo.find().then((todos)=>{
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id))
+  {
+    res.status(404).send({});
+  }
+  else{
+    Todo.findById(id).then((result=>{
+      if(result)
+      {
+        res.status(200).send(result);
+      }
+      else
+      {
+        res.status(404).send(result);
+      }
+      
+    })).catch((err)=>{
+      res.status(400).send();
+    })
+  }
+   
 });
 
 module.exports = {app};
