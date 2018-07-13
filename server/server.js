162cb55ca69e517a339514fbf2989bac011cd189
maxfnine <maxfnine@gmail.com>
2018-07-13 19:32:02 +0300

    Fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./../server/models/todo');
+var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
