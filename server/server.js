2c73d5308608b1f4679f10e53c61fc0780f12356
maxfnine <maxfnine@gmail.com>
2018-07-13 23:10:50 +0300

    Fix 2310

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo.js');
-var { User } = require('./models/user.js');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
